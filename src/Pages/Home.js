6218de3a3d4a1f0db89e095a5406cbe00cec973c
Furkan <furkan.sunger@outlook.com>
2021-08-11 16:48:37 +0300

    First commit

---
+++ src/Pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Menu from '../components/Menu/Menu'
+import Header from '../components/Header/Header'
+import Footer from '../components/Footer/Footer'
+
+const Home = () => {
+    return (
+        <>
+            <Menu />
+            <Header />
+            <Footer />
+        </>
+    )
+}
+
+export default Home
